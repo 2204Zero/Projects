c12308f70cca0cc5569287470971283fc3157fd1
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2025-07-18 15:18:13 +0530

    SMS started

---
+++ TechSpace/my-app/script.js
@@ -0,0 +1,16 @@
+console.log("Hello I am ZERO")
+
+// let age = 25;
+
+// // if(age>18){
+// //     console.log("You can vote")
+// // }else{
+// //     console.log("You cannot vote")
+// // }
+// for(let i =0;i<=10;i++){
+//     console.log(age)
+// }
+
+function hello(name) {
+    console.log("hey"+name+"your welcome to this server!!")
+}
